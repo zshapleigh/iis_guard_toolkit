02a12ccdcb7728c2cbc109ac29a2b8ce4f5f0634
zshapleigh <zshapleigh@yahoo.com>
2025-07-03 23:51:49 -0400

    build out cache check class
    build out cache check class

--- IISGuard/obj/Debug/net8.0/IISGuard.AssemblyInfo.cs
+++ IISGuard/obj/Debug/net8.0/IISGuard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IISGuard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9618502d917801ca2e35795e2b827928bcfe5caf")]
 [assembly: System.Reflection.AssemblyProductAttribute("IISGuard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IISGuard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
